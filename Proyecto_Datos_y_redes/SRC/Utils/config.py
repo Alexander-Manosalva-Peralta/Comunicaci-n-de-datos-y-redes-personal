3efc75660cc9fdb600e5c781a363f59fe3ccdffa
Alexander Manosalva Peralta <yojan.manosalva@upch.pe>
2024-07-02 22:13:29 -0500

    config

---
+++ Proyecto_Datos_y_redes/SRC/Utils/config.py
@@ -0,0 +1,9 @@
+queue_urls = [
+    'https://sqs.us-east-1.amazonaws.com/105371261454/MiColaSQS3',
+    'https://sqs.us-east-1.amazonaws.com/105371261454/MiColaSQS4',
+    'https://sqs.us-east-1.amazonaws.com/105371261454/MiColaSQS5',
+    'https://sqs.us-east-1.amazonaws.com/105371261454/MiColaSQS6',
+    'https://sqs.us-east-1.amazonaws.com/105371261454/MiColaSQS7'
+]
+TOPIC_ARN = 'arn:aws:sns:us-east-1:105371261454:MiTopicoSNS'
+STREAM_NAME = 'MiStreamKinesis'
